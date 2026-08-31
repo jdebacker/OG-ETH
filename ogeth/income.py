7668d73a7cbb560af3c5ca04026ae56b55c12bf4
Jason DeBacker <jdebacker@users.noreply.github.com>
2026-08-31 12:59:02 -0400

    format

--- ogeth/income.py
+++ ogeth/income.py
@@ -116,13 +116,7 @@ def get_e_interp(
     )
     a = x.root
     e_new = usa_params.e[0, :, :] * np.exp(a * usa_params.e[0, :, :])
-    emat_new_scaled = (
-        e_new
-        / (
-            e_new
-            * usa_params.omega_SS
-        ).sum()
-    )
+    emat_new_scaled = e_new / (e_new * usa_params.omega_SS).sum()
     # Now interpolate for the cases where S and/or J not the same in the
     # country parameterization as in the default USA parameterization
     if (
@@ -181,10 +175,7 @@ def get_e_interp(
             (new_s_mesh, new_j_mesh),
             method="linear",
         )
-        emat_new_scaled = (
-            emat_new
-            / (emat_new * age_wgts).sum()
-        )
+        emat_new_scaled = emat_new / (emat_new * age_wgts).sum()
 
         if plot_path is not None:
             kwargs = {"path": plot_path, "filesuffix": "_intrp_scaled"}
